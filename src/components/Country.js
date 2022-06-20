e5ac6937f85ad3e209801fae805589cd15fcca1b
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2022-06-21 02:21:02 +0400

    Update Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -33,7 +33,7 @@ const Wrapper = styled.div`
   .info {
     display: flex;
     flex-direction: column;
-    /* align-items: center; */
+
     justify-content: space-between;
     width: 100%;
     height: 400px;
@@ -69,5 +69,11 @@ const Wrapper = styled.div`
     color: var(--grey-800);
     margin-left: 10px;
   }
+@media (max-width:375px){
+.info{
+height:100%;
+}
+}
+
 `;
 export default Country;
